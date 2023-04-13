52b0741f9b1372f2f6bae887d20bc11b480457d4
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2023-04-13 12:42:53 +0100

    local

--- lrs_system/settings.py
+++ lrs_system/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lrs_db',
-        'HOST': '%',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'abdulmujeeb',
