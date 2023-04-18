2dca8d3a1ebc034c283514de3404d1f514a53670
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2023-04-18 10:27:19 +0100

    non config

--- lrs_system/settings.py
+++ lrs_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config 
+
 
 import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-c4lqwl4sp7f^=n_)d^(e$r#ln&q^3$r33g*77o=4vl1k095gc4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'lrs_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('NAME'),
-        'HOST': config('HOST'),
+        'NAME': 'railway',
+        'HOST': 'containers-us-west-203.railway.app',
         'PORT': '5662',
-        'USER': config('USER'),
-        'PASSWORD': config("PASSWORD"),
+        'USER': 'root',
+        'PASSWORD': ' 0xclbZ7IWUO67JgSmNOT',
     }
 }
 
